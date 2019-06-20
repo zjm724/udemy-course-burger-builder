5fec37afe37cd6e287e70ae94644374e31aaaca0
jieminz <zjm724@hotmail.com>
2019-06-19 23:30:29 -0700

    add and remove ingredients (using disabledInfo, improved version compare to original)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
@@ -15,6 +22,50 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
+        },
+        totalPrice: 4
+    }
+
+    disabledInfo = {
+        ...this.state.ingredients
+    };
+    
+    constructor() {
+        super();
+        const keys = Object.keys(this.disabledInfo);
+        for (let i = 0; i < keys.length; i++) {
+            this.disabledInfo[keys[i]] = true;
+        };
+    };
+
+    addIngredientHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        const updatedCount = this.state.ingredients[type] + 1;
+        updatedIngredients[type] = updatedCount;
+
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        if (this.disabledInfo[type] === true) {
+            this.disabledInfo[type] = false;
+        };
+    }
+
+    removeIngredientHandler = (type) => {
+        if (this.state.ingredients[type] > 0) {
+            const updatedIngredients = {
+                ...this.state.ingredients
+            };
+            const updatedCount = this.state.ingredients[type] - 1;
+            updatedIngredients[type] = updatedCount;
+            
+            const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+            this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+
+            if (updatedCount === 0) {
+                this.disabledInfo[type] = true;
+            };
         }
     }
 
@@ -22,7 +73,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={this.disabledInfo} />
             </Aux>
         );
     }
