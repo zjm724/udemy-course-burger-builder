df2f6f2e70cd769a25ef3fc09e7e4e9a8219e9ea
jieminz <zjm724@hotmail.com>
2019-06-25 15:08:21 -0700

    add sidedrawer, reuse backdrop and beautify it with button and icon

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './Logo.css';
 import BurgerLogo from '../../assets/Images/burger-logo.png';
 
 const Logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={BurgerLogo} alt="MyBurger" />
     </div>
 );
