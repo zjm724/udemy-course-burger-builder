45e596502e31fe54cd6f63d46a087bf4c2e58f02
jieminz <zjm724@hotmail.com>
2019-06-25 16:23:27 -0700

    Improve performance based on compenent hierarchy and shouldComponentUpdate lifecycle method etc.

--- src/components/Burger/BuildControls/OrderSummary/OrderSummary.js
+++ src/components/Burger/BuildControls/OrderSummary/OrderSummary.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import Aux from '../../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../../hoc/Aux/Aux';
 import Button from '../../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return ( 
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
+class OrderSummary extends Component {
+    // Could be a functional component without checking componentWillUpdate since the parent component (Modal) controls the update
+    // componentWillUpdate() {
+    //     console.log('[OrderSummary] WillUpdate');
+    // }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return ( 
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total price: {this.props.totalPrice}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
         );
-    });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total price: {props.totalPrice}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-};
+    };
+}
 
 export default OrderSummary;
